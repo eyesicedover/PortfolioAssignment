7ab30756519b3d8eceab8e14c7d71b476d8c2c78
Stephanie Faber <eyesicedover@gmail.com>
2018-05-04 16:52:54 -0700

    work on posts

--- PortfolioAssignment/Controllers/PostsController.cs
+++ PortfolioAssignment/Controllers/PostsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioAssignment.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,13 @@ namespace PortfolioAssignment.Controllers
 {
     public class PostsController : Controller
     {
+        public AppDbContext db;
+        public UserManager<ApplicationUser> userManager;
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(db.Posts);
         }
     }
 }
