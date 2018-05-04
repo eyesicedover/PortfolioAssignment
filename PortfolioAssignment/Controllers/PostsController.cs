cf66cb8fa05cb5d6d8779ac107db6699661d87d8
Stephanie Faber <eyesicedover@gmail.com>
2018-05-04 12:36:53 -0700

    authentication works

--- PortfolioAssignment/Controllers/PostsController.cs
+++ PortfolioAssignment/Controllers/PostsController.cs
@@ -4,18 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace PortfolioAssignment.Controllers
 {
-    public class HomeController : Controller
+    public class PostsController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-
             return View();
         }
     }
